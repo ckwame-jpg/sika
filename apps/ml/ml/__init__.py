54d57a7fe5f6422d49a713d6840bcec8389bc680
Christopher Prempeh <127361482+ckwame-jpg@users.noreply.github.com>
2026-05-13 19:33:11 -0500

    refactor(ml): drop stale apps/ml/ml/families.py registry (bug #41) (#70)

--- apps/ml/ml/__init__.py
+++ apps/ml/ml/__init__.py
@@ -1,12 +1,9 @@
-from ml.families import DIRECT_PARLAY_FAMILIES, SINGLE_LEG_FAMILIES
 from ml.manifest import ModelArtifact, ModelManifest
 from ml_features import FeatureSpec, vectorize
 
 __all__ = [
-    "DIRECT_PARLAY_FAMILIES",
     "FeatureSpec",
     "ModelArtifact",
     "ModelManifest",
-    "SINGLE_LEG_FAMILIES",
     "vectorize",
 ]
